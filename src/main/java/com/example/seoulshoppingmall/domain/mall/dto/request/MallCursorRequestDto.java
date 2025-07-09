c3d8f18d657dc3871e48cc98f6c9bf5374927190
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-07-09 20:25:52 +0900

    add 커서용 request,responsedto 추가

--- src/main/java/com/example/seoulshoppingmall/domain/mall/dto/request/MallCursorRequestDto.java
+++ src/main/java/com/example/seoulshoppingmall/domain/mall/dto/request/MallCursorRequestDto.java
@@ -1,7 +1,34 @@
 package com.example.seoulshoppingmall.domain.mall.dto.request;
 
 public class MallCursorRequestDto {
+    //마지막으로 조회한 쇼핑몰의 monitoringDate 커서역할
     private String monitoringDateCursor;
+    //마지막으로 조회한 쇼핑몰의 ID (동일 날짜일 경우 정렬 보조)
     private Long idCursor;
+    //한 페이지에 가져올 데이터 수
     private int size = 10;
+    //검색 조건: 평점
+    private Integer overallRating;
+    //검색조건: 사업 상태
+    private String businessStatus;
+    //getter추가
+    public String getMonitoringDateCursor() {
+        return monitoringDateCursor;
+    }
+
+    public Long getIdCursor() {
+        return idCursor;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Integer getOverallRating() {
+        return overallRating;
+    }
+
+    public String getBusinessStatus() {
+        return businessStatus;
+    }
 }
--- src/main/java/com/example/seoulshoppingmall/domain/mall/dto/response/MallCursorResponseDto.java
+++ src/main/java/com/example/seoulshoppingmall/domain/mall/dto/response/MallCursorResponseDto.java
@@ -3,7 +3,9 @@ package com.example.seoulshoppingmall.domain.mall.dto.response;
 import java.util.List;
 
 public class MallCursorResponseDto {
+    //현재 페이지의 쇼핑몰 리스트
     private List<MallResponseDto> malls;
+    //다음 페이지를 위한 커서 정보
     private String nextMonitoringDateCursor;
     private Long nextIdCursor;
 
@@ -11,5 +13,17 @@ public class MallCursorResponseDto {
         this.malls = malls;
         this.nextMonitoringDateCursor = nextdate;
         this.nextIdCursor = nextId;
+    }
+
+    public List<MallResponseDto> getMalls() {
+        return malls;
+    }
+
+    public String getNextMonitoringDateCursor() {
+        return nextMonitoringDateCursor;
+    }
+
+    public Long getNextIdCursor() {
+        return nextIdCursor;
     }
 }
