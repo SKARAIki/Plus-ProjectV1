cb8bc41026c897e4517058ebc6e1efd03838880b
이형준 <xkrhd3@naver.com>
2025-07-11 16:13:27 +0900

    CSV 읽기 및 저장 7초

--- src/main/java/com/example/trendsixtown/domain/Controller/ShoppingMallCsvController.java
+++ src/main/java/com/example/trendsixtown/domain/Controller/ShoppingMallCsvController.java
@@ -1,5 +1,6 @@
 package com.example.trendsixtown.domain.Controller;
 
+import com.example.trendsixtown.common.dto.ApiResponse;
 import com.example.trendsixtown.domain.service.ShoppingMallCsvService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/csv")
@@ -18,10 +21,10 @@ public class ShoppingMallCsvController {
     private final ShoppingMallCsvService shoppingMallCsvService;
 
     @PostMapping("/collection")
-    public ResponseEntity<String> getShoppingMallCSV(@RequestParam("mall")MultipartFile file) {
+    public ResponseEntity<ApiResponse<String>> getShoppingMallCSV(@RequestParam("mall")MultipartFile file) throws IOException {
 
-        String cvs = shoppingMallCsvService.getShoppingMall(file);
+        String cvs = shoppingMallCsvService.uploadCsv(file);
 
-        return new ResponseEntity<>(cvs,HttpStatus.OK);
+        return new ResponseEntity<>(ApiResponse.success(HttpStatus.OK,cvs,"CSV 전송 및 업로드 완료"),HttpStatus.OK);
     }
 }
--- src/main/java/com/example/trendsixtown/domain/service/ShoppingMallCsvService.java
+++ src/main/java/com/example/trendsixtown/domain/service/ShoppingMallCsvService.java
@@ -1,8 +1,11 @@
 package com.example.trendsixtown.domain.service;
 
+import com.example.trendsixtown.common.exception.fileException;
 import com.example.trendsixtown.domain.entity.ShoppingMall;
 import com.example.trendsixtown.domain.repository.ShoppingMallCsvRepository;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.result.UpdateCountOutput;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +16,12 @@ import java.io.InputStreamReader;
 import java.lang.module.FindException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,84 +29,136 @@ import java.util.List;
 public class ShoppingMallCsvService {
 
     private final ShoppingMallCsvRepository shoppingMallCsvRepository;
+    //데이터를 담을 수 있는 최대 사이즈 선언
+    private static final int CHUNK_SIZE  = 2000;
+    private static final int THREAD_COUNT = 4;
 
-    //클라이언트가 업로드 한 CSV 파일 받는다
-    public String getShoppingMall(MultipartFile file) {
 
-        //ShoppingMall타입으로 리스트 선언
-        //엔티티 타입의 값만 반영 가능
-        List<ShoppingMall> shoppingMallList = new ArrayList<>();
-        //데이터를 담을 수 있는 최대 사이즈 선언
-        final int BATCH_SIZE = 1000;
+    public String uploadCsv(MultipartFile file) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+        List<Future<?>> futures = new ArrayList<>();
 
-        //받은 파일이 내용이 비워져 있으면 예외처리
-        if (file.isEmpty()) {
-            throw new FindException();
-        }
-        //파일을 한줄 씩 읽기 가능하도록 설정
-        //받은 CSV 파일 바이트 형태로 받은 후 해당 파일을 문자열로 변환 그 후 버퍼링을 통해 효율적으로 읽도록 설정
+        List<String> allLines;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
 
-            //라인 변수 선언
-            String line;
-            //지금 읽는 줄이 첫번째 줄인지 여부 판단하는 boolean플래그 변수 활성화
-            boolean isFirst = true;
-            //가져온 파일 이름 가져오기
-            String fileName = file.getName();
+            allLines =
+                    reader.lines().
+                            skip(1).
+                            collect(Collectors.toList());
 
-            //첫번째 읽은 값 받은 후 continue로 건너뛴 후 플래그 변수 비활성화
-            //받은 문자열 파일 readLine을 통해
-            while ((line = reader.readLine()) != null) {
-                if (isFirst) {
-                    isFirst = false;
-                    continue;
-                }
-
-                //필드 내 값이 없어도 한 공간으로 판단해서 리스트에 반영 설정 -1이
-                //없으면 값 있는 필드만 리스트에 반영
-                String[] fields = line.split(",",-1);
-
-                //열이 30개 미만이면 아래 로직 건너뜀 이상이면 진행
-                if (fields.length < 33) continue;
-
-                //trim(공백제거)
-                //엔티티에 저장된 값 변수에 저장
-                ShoppingMall shoppingMall = new ShoppingMall(
-                        //문자열을 Long 타입으로 변환 & 문자열을 int 타입으로 변환
-                        //Long.parseLong(fields[0]),Integer.getInteger(fields[0])
-                        fields[22], fields[11], fields[0], fields[9], fields[6],
-                        fields[17], fields[13], fields[25], fields[8], fields[28],
-                        fields[26], fields[2], fields[7], fields[16], fields[1],
-                        fields[18], fields[29], fields[31], fields[4], fields[10],
-                        fields[13], fields[19], fields[3], fields[21], fields[15],
-                        fields[23], fields[27], fields[5], fields[24], fields[30],
-                        fields[20], fields[14]
-                );
-
-                //엔티티에 저장한 데이터 리스트에 반영
-                shoppingMallList.add(shoppingMall);
-
-                //리스트 사이즈가 rageSize보다 크거나 같을때 저장/같지 않거나 작을때 저장 종료
-                if(shoppingMallList.size() >= BATCH_SIZE){
-                    //데이터베이스에 리스트 내 데이터 저장
-                    shoppingMallCsvRepository.saveAll(shoppingMallList);
-                    //모든 요소만 제거(주의 : 내부에 모든 값이 제거되어 사이즈가 0이 됨 isEmpty도 true됨
-                    //리스트가 null이 되는것은 아님 빈 공간이라도 있으니
-                    shoppingMallList.clear();
-                }
-            }
-
-            //리스트에 값이 존재할때 저장 진행/값이 없을때 저장 종료
-            if(!shoppingMallList.isEmpty()){
-                //남은 데이터 마무리로 저장
-                shoppingMallCsvRepository.saveAll(shoppingMallList);
-            }
-
-            //결과 반환
-            return fileName + "을 업로드 및 저장하였습니다.";
-        } catch (IOException e) {
-            throw new FindException();
         }
+        List<List<String>> chunks = splitIntoChunks(allLines,CHUNK_SIZE);
+
+        for(List<String> chunk : chunks){
+            futures.add(executorService.submit(()->{
+                List<ShoppingMall> entites = chunk.stream()
+                        .map(this::getShoppingMall)
+                        .filter(Objects::nonNull)
+                        .toList();
+
+                shoppingMallCsvRepository.saveAll(entites);
+            }));
+        }
+
+        for(Future<?> future : futures){
+            try {
+                future.get();
+            } catch (Exception e){
+               e.printStackTrace();
+            }
+        }
+
+        executorService.shutdown();
+
+        return allLines.size()+"건";
+    }
+
+    private List<List<String>> splitIntoChunks(List<String> liest,int chunkSize){
+        List<List<String>> chunks = new ArrayList<>();
+
+        for(int i = 0 ; i < liest.size() ; i += chunkSize){
+            chunks.add(liest.subList(i,Math.min(i+chunkSize, liest.size())));
+        }
+        return chunks;
+    }
+
+
+    //클라이언트가 업로드 한 CSV 파일 받는다
+    public ShoppingMall getShoppingMall(String line) {
+
+        //List<Future<?>> futures = new ArrayList<>();
+        //ShoppingMall타입으로 리스트 선언
+        //엔티티 타입의 값만 반영 가능
+        //List<ShoppingMall> shoppingMallList = new ArrayList<>();
+        //라인 변수 선언
+        //List<String> lines;
+        //String line;
+
+        //받은 파일이 내용이 비워져 있으면 예외처리
+//        if (file.isEmpty()) {
+//            throw new FindException();
+//        }
+
+        //파일을 한줄 씩 읽기 가능하도록 설정
+        //받은 CSV 파일 바이트 형태로 받은 후 해당 파일을 문자열로 변환 그 후 버퍼링을 통해 효율적으로 읽도록 설정
+        //try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+        //지금 읽는 줄이 첫번째 줄인지 여부 판단하는 boolean플래그 변수 활성화
+        //boolean isFirst = true;
+        //가져온 파일 이름 가져오기
+        //String fileName = file.getName();
+
+        //첫번째 읽은 값 받은 후 continue로 건너뛴 후 플래그 변수 비활성화
+        //받은 문자열 파일 readLine을 통해
+//            while ((line = reader.readLine()) != null) {
+//                if (isFirst) {
+//                    isFirst = false;
+//                    continue;
+//                }
+
+        //필드 내 값이 없어도 한 공간으로 판단해서 리스트에 반영 설정 -1이
+        //없으면 값 있는 필드만 리스트에 반영
+        String[] fields = line.split(",", -1);
+
+        //열이 30개 미만이면 아래 로직 건너뜀 이상이면 진행
+        if (fields.length < 33)
+            //trim(공백제거)
+            //엔티티에 저장된 값 변수에 저장
+            return new ShoppingMall(
+                    //문자열을 Long 타입으로 변환 & 문자열을 int 타입으로 변환
+                    //Long.parseLong(fields[0]),Integer.getInteger(fields[0])
+                    fields[22].replace("\"", ""), fields[11].replace("\"", ""), fields[0], fields[9], fields[6],
+                    fields[17], fields[13], fields[25], fields[8], fields[28],
+                    fields[26], fields[2], fields[7], fields[16], fields[1],
+                    fields[18], fields[29], fields[31], fields[4], fields[10],
+                    fields[13], fields[19], fields[3], fields[21], fields[15],
+                    fields[23], fields[27], fields[5], fields[24], fields[30],
+                    fields[20], fields[14]
+            );
+
+
+
+        //엔티티에 저장한 데이터 리스트에 반영
+        //shoppingMallList.add(shoppingMall);
+
+        //리스트 사이즈가 rageSize보다 크거나 같을때 저장/같지 않거나 작을때 저장 종료
+//                if(shoppingMallList.size() >= BATCH_SIZE){
+//                    //데이터베이스에 리스트 내 데이터 저장
+//                    shoppingMallCsvRepository.saveAll(shoppingMallList);
+//                    shoppingMallList.clear();
+//                }
+
+        //리스트에 값이 존재할때 저장 진행/값이 없을때 저장 종료
+//            if(!shoppingMallList.isEmpty()){
+//                //남은 데이터 마무리로 저장
+//                shoppingMallCsvRepository.saveAll(shoppingMallList);
+//                //모든 요소만 제거(주의 : 내부에 모든 값이 제거되어 사이즈가 0이 됨 isEmpty도 true됨
+//                //리스트가 null이 되는것은 아님 빈 공간이라도 있으니
+//                shoppingMallList.clear();
+//            }
+        //null사용한 이유는 어차피 읽기 전용 저수준 메서드이기때문(저수준 메서드) 활용
+        //비지니스 로직을 담는 도메인과 같은 고수준의 모듈에서는 null 절대 사용 금지
+        return null;
     }
 }
 // 1분 9초가 한계... > 이후 피드백 받고 진행
