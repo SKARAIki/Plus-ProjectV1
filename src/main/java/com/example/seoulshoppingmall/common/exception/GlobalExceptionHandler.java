84cf5a84ff37a5353b9e9e391ac67b347e13d7e4
YeJin420 <lyjin030396@gmail.com>
2025-07-11 17:40:37 +0900

    feat : 예외처리 순서 변경

--- src/main/java/com/example/seoulshoppingmall/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/seoulshoppingmall/common/exception/GlobalExceptionHandler.java
@@ -1,30 +1,25 @@
 package com.example.seoulshoppingmall.common.exception;
 
+import com.example.seoulshoppingmall.common.dto.ErrorResponse;
 import com.example.seoulshoppingmall.domain.mall.exception.OpenApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    // OpenAPI 호출 실패
+    // 예를 들어 외부 OpenAPI 호출 중 오류가 발생한 경우
     @ExceptionHandler(OpenApiException.class)
-    public ResponseEntity<?> handleOpenApiCallException(OpenApiException e) {
+    public ResponseEntity<ErrorResponse> handleOpenApiCallException(OpenApiException openApiException) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of(
-                        "status", 500,
-                        "message", e.getMessage()
-                ));
+                .body(new ErrorResponse(500, openApiException.getMessage()));
     }
 
+    // 예상하지 못한 예외가 발생한 경우에도 사용자에게 500 에러로 응답 -> 메시지로 처리
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of(
-                        "status", 500,
-                        "message", "서버 오류가 발생했습니다."
-                ));
+                .body(new ErrorResponse(500, "서버 오류가 발생했습니다."));
     }
 }
